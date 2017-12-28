2ef59555ff60a20df4a68052c1d7b39e7ec1e4ab
Karen Posada <k100posada@gmail.com>
2017-12-27 23:20:20 -0600

    Add Get /todos/:id

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,38 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '5a440ae0d244302dad53800e';
+
+// if (!ObjectID.isValid(id)) {
+//     console.log('ID not valid');
+// }
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+User.findById('5a43e22cbb2fa11cf2e003c1')
+    .then((user) => {
+        if(!user) {
+            return console.log('Unable to find user');
+        }
+        console.log(JSON.stringify(user, undefined, 2))
+    }, (e) => {
+        console.log(e);
+    });
